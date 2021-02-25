14058e9fb9df21d0586e4e68b2856de79dc1ab6f
Prineth Fernando <prinethfernandox@gmail.com>
2021-02-25 10:15:40 +0530

    update

---
+++ Day-1/Q1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Q1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass obj = new MyClass();
+            obj.MyFunction();
+
+            Console.Read();
+        }
+    }
+}
